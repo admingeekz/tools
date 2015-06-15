f8e39038e07bd64d0fb7381e01da55d2d49aa466
Scott Mcintyre <me@scott.cm>
2015-06-15 23:16:48 +0100

    Adding info and license

--- wp-scanner.py
+++ wp-scanner.py
@@ -1,4 +1,29 @@
 #!/usr/bin/python
+# WordPress Version Scanner
+# v1.1
+# Scan paths for outdated WordPress installations
+# If MySQLdb available then wp-config.php credentials will be checked for valid installations
+#
+# URL: www.admingeekz.com
+# Contact: sales@admingeekz.com
+#
+#
+# Copyright (c) 2013, AdminGeekZ Ltd
+# All rights reserved.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License, version 2, as
+# published by the Free Software Foundation.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+#
 import sys
 #Check for version 2.3 or greater
 if int(sys.version_info[1] <= 2):
